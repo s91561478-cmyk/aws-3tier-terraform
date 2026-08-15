f7aac93a193b4747c2e2d91a146db526c1e95347
JINSIK SHIN <s91561478@gmail.com>
2026-08-15 17:13:49 +0900

    Add WAF Admin IP Address

--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "db_password" {
   type        = string
   sensitive   = true
 }
+
+variable "waf_admin_ip_address" {
+  description = "waf-admin ip address"
+  type        = list(string)
+}
