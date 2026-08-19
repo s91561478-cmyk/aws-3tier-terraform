8d04fd897317873b26d70d79430bf0d6126aba84
JINSIK SHIN <s91561478@gmail.com>
2026-08-19 20:27:16 +0900

    Add Terget Group ARN or ALB ARN

--- modules/alb/output.tf
+++ modules/alb/output.tf
@@ -2,8 +2,16 @@
 # Target Group ARN
 # ============================================================
 output "target_group_arn" {
-  description = "Target Group ARN"                # Output description
-  value       = aws_lb_target_group.ecom_dev_alb_tg.arn # Target Group ARN
+  description = "Target Group ARN"
+  value       = aws_lb_target_group.ecom_dev_alb_tg.arn
+}
+
+# ============================================================
+# Target Group Arn Suffix
+# ============================================================
+output "target_group_arn_suffix" {
+  description = "Target Group Arn Suffix"
+  value       = aws_lb_target_group.ecom_dev_alb_tg.arn_suffix
 }
 
 # ============================================================
@@ -28,4 +36,12 @@ output "alb_zone_id" {
 output "alb_arn" {
   description = "ALB ARN"
   value       = aws_lb.ecom_dev_alb.arn
+}
+
+# ============================================================
+# ALB ARN Suffix
+# ============================================================
+output "alb_arn_suffix" {
+  description = "ALB ARN Suffix"
+  value       = aws_lb.ecom_dev_alb.arn_suffix
 }
