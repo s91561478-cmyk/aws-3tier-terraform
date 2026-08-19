529626cc0a8b4ced2ce979bb69f394a8fbfc6165
JINSIK SHIN <s91561478@gmail.com>
2026-08-19 20:31:10 +0900

    Add SNS Email or EC2 Instance ID for disk alarm

--- variables.tf
+++ variables.tf
@@ -8,3 +8,13 @@ variable "waf_admin_ip_address" {
   description = "waf-admin ip address"
   type        = list(string)
 }
+
+variable "sns_email" {
+  description = "SNS Email"
+  type        = string
+}
+
+variable "ec2_instance_id" {
+  description = "EC2 Instance ID for disk alarm"
+  type        = string
+}
